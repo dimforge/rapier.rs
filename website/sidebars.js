d12cd94601aa2ebe5b807cf6a7b751735445e90e
Sébastien Crozet <developer@crozet.re>
2020-08-24 21:03:46 +0200

    Add section about the bevy_rapier dependency.

--- website/sidebars.js
+++ website/sidebars.js
@@ -6,9 +6,12 @@ module.exports = {
       'Rust': [
         'user_guides/rust/getting_started',
         'user_guides/rust/rigid_body_simulation',
-        'user_guides/rust/integration_parameters',
-        'user_guides/rust/integration_to_bevy'
+        'user_guides/rust/integration_parameters'
       ],
+      'Bevy Plugin': [
+        'user_guides/bevy_plugin/getting_started'
+      ],
+
       'JavaScript': [ 'user_guides/javascript/getting_started' ],
     }],
     'API Documentation': [ 'api/javascript/JavaScript2D', 'api/javascript/JavaScript3D' ],
