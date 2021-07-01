aad11e13c30afd9a918834e832516ce60b77face
Crozet Sébastien <developer@crozet.re>
2021-07-01 11:01:06 +0200

    Split the colliders and rigid_bodies user-guide file into multiple files

--- website/publish.sh
+++ website/publish.sh
@@ -5,4 +5,4 @@ PUBLISH_MODE=1 yarn build
 cp .htaccess build/.
 rsync -av --delete-after build/ crozet@ssh.cluster003.hosting.ovh.net:/home/crozet/rapier/
 rm docs/user_guides/rust/*.mdx
-rm docs/user_guides/rust_bevy_plugin/*.mdx
+rm docs/user_guides/bevy_plugin/*.mdx
