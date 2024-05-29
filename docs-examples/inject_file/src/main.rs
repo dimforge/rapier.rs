6083260f1e1834e526d03dc95b37a0c568776d6a
Thierry Berger <contact@thierryberger.com>
2024-05-29 11:11:11 +0200

    added test to inject script

--- docs-examples/inject_file/src/main.rs
+++ docs-examples/inject_file/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{borrow::Cow, fs::read_to_string};
 
 use regex::{Captures, Regex};
 
@@ -10,17 +10,25 @@ fn main() {
     let file: &str = &read_to_string(&input_path)
         .expect(&format!("Could not read file at input {}", input_path));
 
+    let result = injected(file, |file_path| {
+        let mut path = "..".to_string();
+        path.push_str(&file_path);
+        path.to_string()
+    });
+    print!("{result}");
+}
+
+fn injected(source_text: &str, get_path: fn(&str) -> String) -> Cow<str> {
     // Regex to find "<load" tags and capture their info (path + marker)
     let re = Regex::new(r"<load path='(.*)'.*marker='(.*)'.*>").unwrap();
 
-    let result = re.replace_all(file, |caps: &Captures| {
+    let result = re.replace_all(source_text, |caps: &Captures| {
         let infos = &caps.extract::<2>().1;
         assert!(
             infos.len() == 2,
             "load tag encountered without a path or marker"
         );
-        let mut path = "..".to_string();
-        path.push_str(infos[0]);
+        let path = get_path(infos[0]);
         // Reading file from the path of the tag of input file
         let to_inject = read_to_string(&path).expect(&format!("could not read path: {}", path));
 
@@ -39,7 +47,22 @@ fn main() {
                 to_keep.1[0]
             })
             .collect::<Vec<_>>();
+        if to_keep.len() == 0 {
+            eprintln!("ERROR: marker {} not found in {}", infos[1], infos[0]);
+        }
         to_keep.join("")
     });
-    print!("{result}");
+    result
+}
+
+#[test]
+fn simple_injection() {
+    use crate::*;
+
+    let result = injected(
+        "<load path='test/to_inject1.txt' marker='ToInject1_1' />",
+        |path| path.to_string(),
+    );
+    // Trimming end for cross platform, on windows I had \r finishing result.
+    assert_eq!(result.trim_end(), "correct data1 1");
 }
