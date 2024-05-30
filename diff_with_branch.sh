86c7386c442aac133743b235a1a70fdcf1d02c04
Thierry Berger <contact@thierryberger.com>
2024-05-30 11:07:01 +0200

    check local modifications before running diff

--- diff_with_branch.sh
+++ diff_with_branch.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -ve
 
-echo $1
-echo $2
+echo '$1: '$1
+echo '$2: '$2
 
-if [ $1 == "--help" ]; then
+if [[ $1 = "--help" ]]; then
     echo "USAGE: $0 [branch1 [branch2]]
     branch1 default: - (previous branch) FIXME: Currently only supports comparison with a branch without injection.
     branch2 default: . (current branch)
@@ -12,6 +11,15 @@ if [ $1 == "--help" ]; then
     "
     exit;
 fi
+
+if [[ -n $(git status -s) ]]; then
+    echo "you have uncommitted changes, chances are they will be erased while switching branches.
+    Please commit or stash your changes first."
+    git status -s
+    exit;
+fi
+
+set -ve
 
 (
     # "-" in git means previous branch
@@ -23,11 +31,10 @@ fi
 
     FILENAMES=`find docs/user_guides/templates/ -type f`
     OUTPUT_FOLDER=tmp_diff_branches/branch1_templates_injected/ ./inject_code_in_user_guides.sh $FILENAMES
-    exit;
     git checkout $BRANCH_1
     # FIXME: Currently comparing with master, where there is not the injected files.
     # Once master has injected file we'll have to generate them before comparing.
     # find docs/user_guides/templates/ -type f -print0 | OUTPUT_FOLDER=tmp_diff_branches/branch2_templates_injected/ xargs -0 ./inject_code_in_user_guides.sh
     # diff tmp_diff_branches/branch1_templates_injected/ tmp_diff_branches/branch2_templates_injected/ > tmp_total_diff.diff
-    diff --git -w docs/user_guides/templates/ tmp_diff_branches/branch1_templates_injected/ > total_diff.diff
+    diff --git -w docs/user_guides/templates/ tmp_diff_branches/branch1_templates_injected/ > tmp_total_diff.diff
 )
