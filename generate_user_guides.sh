0d60633b28f092134bbfda7bc45ebc7bb503b8b7
Thierry Berger <contact@thierryberger.com>
2024-05-30 16:31:31 +0200

    self review

--- generate_user_guides.sh
+++ generate_user_guides.sh
@@ -5,8 +5,6 @@ mkdir -p docs/user_guides/rust/
 mkdir -p docs/user_guides/bevy_plugin/
 mkdir -p docs/user_guides/javascript/
 
-# NOTE: injecting all code here is suboptimal because we're copying it 3 times
-#       and parsing all those multiple times. Ideally we would inject the code at the last step.
 find docs/user_guides/templates/ -type f -print0 | OUTPUT_FOLDER=docs/user_guides/templates_injected xargs -0 ./inject_code_in_user_guides.sh
 
 cp -r docs/user_guides/templates/* docs/user_guides/rust/.
--- inject_code_in_user_guides.sh
+++ inject_code_in_user_guides.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 set -v
 
-DRY_RUN="${DRY_RUN:-0}"
 KEEP_TMP="${KEEP_TMP:-0}"
 PRINT_DIFF="${PRINT_DIFF:-1}"
-PUBLISH_MODE="${PUBLISH_MODE:-0}"
 
 echo $OUTPUT_FOLDER
-echo $DRY_RUN
 echo $KEEP_TMP
 echo $PRINT_DIFF
-echo $PUBLISH_MODE
 
 
 if [ -z ${OUTPUT_FOLDER:x} ]; then
-    echo "ERROR: MISING: OUTPUT_FOLDER env variable"
+    echo "ERROR: MISSING: OUTPUT_FOLDER env variable"
     exit;
 fi
 
@@ -41,9 +37,9 @@ do
     cargo run --quiet --release "$file_to_inject" > "$file_injected"
     if [ $PRINT_DIFF -eq 1 ]; then
         if cmp -s "$file_to_inject" "$file_injected"; then
-            echo -e "No substitution to $file_to_inject"
+            echo "No substitution to $file_to_inject"
         else
-            echo -e "Substitution to $file_injected"
+            echo "Substitution to $file_injected"
             mkdir -p "$(dirname $diff_file)"
             diff $file_to_inject $file_injected > "$diff_file".diff
         fi
@@ -51,6 +47,5 @@ do
 done
 
 if [ ! $KEEP_TMP ]; then
-    rm -rf tmp_inject
     rm -rf tmp_diff
 fi
