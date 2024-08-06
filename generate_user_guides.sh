731feefbbc65c821c3651fd614b4c1a6fadc6f0c
Thierry Berger <contact@thierryberger.com>
2024-08-06 09:44:15 +0200

    gsed uses variable to work easily on linux

--- generate_user_guides.sh
+++ generate_user_guides.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -v
 
+if [ gsed -v ]; then
+    gsed=gsed
+else
+    # Hopefully installed sed is the gnu one.
+    # note: aliases work poorly with xargs.
+    gsed=sed
+fi
+
 mkdir -p docs/user_guides/rust/
 mkdir -p docs/user_guides/bevy_plugin/
 mkdir -p docs/user_guides/javascript/
@@ -12,28 +20,28 @@ cp -r docs/user_guides/templates_injected/* docs/user_guides/bevy_plugin/.
 cp -r docs/user_guides/templates_injected/* docs/user_guides/javascript/.
 
 find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 gawk -i inplace -v RS='<rapier>[^<]*</rapier>' -v ORS= '1'
-find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 gsed -i '/<rapier>/,/<\/rapier>/d'
+find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 $gsed -i '/<rapier>/,/<\/rapier>/d'
 find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 gawk -i inplace -v RS='<js>[^<]*</js>' -v ORS= '1'
-find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 gsed -i '/<js>/,/<\/js>/d'
-find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 gsed -i 's\<bevy>\\g'
-find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 gsed -i 's\</bevy>\\g'
-find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 gsed -i 's\<notjs>\\g'
-find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 gsed -i 's\</notjs>\\g'
+find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 $gsed -i '/<js>/,/<\/js>/d'
+find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 $gsed -i 's\<bevy>\\g'
+find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 $gsed -i 's\</bevy>\\g'
+find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 $gsed -i 's\<notjs>\\g'
+find docs/user_guides/bevy_plugin/ -type f -print0 | xargs -0 $gsed -i 's\</notjs>\\g'
 
 find docs/user_guides/rust/ -type f -print0 | xargs -0 gawk -i inplace -v RS='<bevy>[^<]*</bevy>' -v ORS= '1'
-find docs/user_guides/rust/ -type f -print0 | xargs -0 gsed -i '/<bevy>/,/<\/bevy>/d'
+find docs/user_guides/rust/ -type f -print0 | xargs -0 $gsed -i '/<bevy>/,/<\/bevy>/d'
 find docs/user_guides/rust/ -type f -print0 | xargs -0 gawk -i inplace -v RS='<js>[^<]*</js>' -v ORS= '1'
-find docs/user_guides/rust/ -type f -print0 | xargs -0 gsed -i '/<js>/,/<\/js>/d'
-find docs/user_guides/rust/ -type f -print0 | xargs -0 gsed -i 's\<rapier>\\g'
-find docs/user_guides/rust/ -type f -print0 | xargs -0 gsed -i 's\</rapier>\\g'
-find docs/user_guides/rust/ -type f -print0 | xargs -0 gsed -i 's\<notjs>\\g'
-find docs/user_guides/rust/ -type f -print0 | xargs -0 gsed -i 's\</notjs>\\g'
+find docs/user_guides/rust/ -type f -print0 | xargs -0 $gsed -i '/<js>/,/<\/js>/d'
+find docs/user_guides/rust/ -type f -print0 | xargs -0 $gsed -i 's\<rapier>\\g'
+find docs/user_guides/rust/ -type f -print0 | xargs -0 $gsed -i 's\</rapier>\\g'
+find docs/user_guides/rust/ -type f -print0 | xargs -0 $gsed -i 's\<notjs>\\g'
+find docs/user_guides/rust/ -type f -print0 | xargs -0 $gsed -i 's\</notjs>\\g'
 
 find docs/user_guides/javascript/ -type f -print0 | xargs -0 gawk -i inplace -v RS='<rapier>[^<]*</rapier>' -v ORS= '1'
-find docs/user_guides/javascript/ -type f -print0 | xargs -0 gsed -i '/<rapier>/,/<\/rapier>/d'
+find docs/user_guides/javascript/ -type f -print0 | xargs -0 $gsed -i '/<rapier>/,/<\/rapier>/d'
 find docs/user_guides/javascript/ -type f -print0 | xargs -0 gawk -i inplace -v RS='<bevy>[^<]*</bevy>' -v ORS= '1'
-find docs/user_guides/javascript/ -type f -print0 | xargs -0 gsed -i '/<bevy>/,/<\/bevy>/d'
+find docs/user_guides/javascript/ -type f -print0 | xargs -0 $gsed -i '/<bevy>/,/<\/bevy>/d'
 find docs/user_guides/javascript/ -type f -print0 | xargs -0 gawk -i inplace -v RS='<notjs>[^<]*</notjs>' -v ORS= '1'
-find docs/user_guides/javascript/ -type f -print0 | xargs -0 gsed -i '/<notjs>/,/<\/notjs>/d'
-find docs/user_guides/javascript/ -type f -print0 | xargs -0 gsed -i 's\<js>\\g'
-find docs/user_guides/javascript/ -type f -print0 | xargs -0 gsed -i 's\</js>\\g'
+find docs/user_guides/javascript/ -type f -print0 | xargs -0 $gsed -i '/<notjs>/,/<\/notjs>/d'
+find docs/user_guides/javascript/ -type f -print0 | xargs -0 $gsed -i 's\<js>\\g'
+find docs/user_guides/javascript/ -type f -print0 | xargs -0 $gsed -i 's\</js>\\g'
